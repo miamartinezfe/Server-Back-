adca0aa71feb95ce24021a7d166194f623c6fb7c
miamartinezfe <miguelshaddix95@hotmail.com>
2023-04-06 18:23:24 -0500

    Creacion Server

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3001;
+const data = require ("./utils/data");
+
+exports.modules = http
+  .createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); //permitimos request de todo el mundo
+    const { url } = req;
+    if (url.includes("/rickandmorty/character/")) {
+      const aux = url.split("/");
+      var id = Number(aux[aux.length - 1]);
+    }
+    switch (url) {
+      case `/rickandmorty/character/${id}`: {
+        const char = data.filter((char) => {
+            console.log("este es char ",char);
+            return char.id === id;
+          });       
+        res.writeHead(200, { "contentType": "application/json" });
+        res.end(JSON.stringify(char[0]));
+        return;
+      }
+
+      default:
+        res.writeHead(404, { contentType: "text/plain" });
+        res.end("No existe la url");
+        return;
+    }
+  })
+  .listen(PORT, "localhost");
